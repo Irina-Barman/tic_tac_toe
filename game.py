56dd12b51191f0f4a1a90320dda6fe580cfee704
Ирина Барман <attame@yandex.ru>
2024-06-24 10:56:02 +0500

    смена игроков после каждого хода

--- game.py
+++ game.py
@@ -1,43 +1,55 @@
+# game.py
+
 from gameparts import Board
 from gameparts.exceptions import FieldIndexError
 
 
 def main():
     game = Board()
+    # Первыми ходят крестики.
+    current_player = 'X'
+    # Это флаговая переменная. По умолчанию игра запущена и продолжается.
+    running = True
     game.display()
 
+    # Тут запускается основной цикл игры.
+    while running:
 
-# Запускается бесконечный цикл.
-    while True:
-        # В этом блоке содержатся значения, которые могут вызвать исключение.
-        try:
-            # Ползователь вводит номер строки.
-            row = int(input('Введите номер строки: '))
-            # Если введенное число меньше 0 или больше
-            # или равно field_size...
-            if row < 0 or row >= game.field_size:
-                # ... и выбразывается собственное исключение FieldIndexError
-                raise FieldIndexError
-            column = int(input('Введите номер столбца: '))
-            if column < 0 or column >= game.field_size:
-                raise FieldIndexError
-        except FieldIndexError:
-            print(
-                'Значение добжно быть неотрицательным и меньше '
-                f' {game.field_size}.'
-            )
-            print('Пожалуйста, введите значение для строки и столбца заново')
-            continue
-        except ValueError:
-            print('Буквы вводить нельзя, только цифры')
-            print('Пожалуйста, введите значение для строки и столбца заново')
-        else:
-            break
+        print(f'Ход делают {current_player}')
 
-    game.make_move(0, 2, 'X')
-    print('Ход сделан!')
-    # Перерисовать поле с учётом сделанного хода.
-    game.display()
+        # Запускается бесконечный цикл.
+        while True:
+            try:
+                row = int(input('Введите номер строки: '))
+                if row < 0 or row >= game.field_size:
+                    raise FieldIndexError
+                column = int(input('Введите номер столбца: '))
+                if column < 0 or column >= game.field_size:
+                    raise FieldIndexError
+            except FieldIndexError:
+                print(
+                    'Значение должно быть неотрицательным и меньше '
+                    f'{game.field_size}.'
+                )
+                print('Введите значения для строки и столбца заново.')
+                continue
+            except ValueError:
+                print('Буквы вводить нельзя. Только числа.')
+                print('Введите значения для строки и столбца заново.')
+                continue
+            except Exception as e:
+                print(f'Возникла ошибка: {e}')
+            else:
+                break
+
+        # Теперь для установки значения на поле само значение берётся
+        # из переменной current_player.
+        game.make_move(row, column, current_player)
+        game.display()
+        # Тернарный оператор, через который реализована смена игроков.
+        # Если current_player равен X, то новым значением будет O,
+        # иначе — новым значением будет X.
+        current_player = 'O' if current_player == 'X' else 'X'
 
 
 if __name__ == '__main__':
